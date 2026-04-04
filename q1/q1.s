cd228d5e7ec2c7f1628541255e8dbab3502e956b
sidharthv-maker <siddharth08vutukuru@gmail.com>
2026-04-04 15:49:22 +0530

    Use call malloc

--- q1/q1.s
+++ q1/q1.s
@@ -1,11 +1,8 @@
-.data
-heap_ptr: .dword 0x10010000 
 .text
 .globl make_node
 .globl insert
 .globl get
 .globl getAtMost
- 
 # Function: make_node
 # Arguments: a0 = val (int)
 # Returns: a0 = pointer to new Node
@@ -13,21 +10,19 @@ make_node:
     addi sp, sp, -32
     sd ra, 24(sp)
     sd s0, 16(sp)
-    sd s1, 8(sp)
-    mv s0, a0
+    mv s0, a0   
+    
     li a0, 24
-    jal allocate_memory
-    mv s1, a0
-    sw s0, 0(s1)
-    sd zero, 8(s1)
-    sd zero, 16(s1)
-    mv a0, s1
-    ld s1, 8(sp)
+    call malloc
+    # Set node->val = val (4 bytes at offset 0)
+    sw s0, 0(a0)
+    sd zero, 8(a0)
+    sd zero, 16(a0)
     ld s0, 16(sp)
     ld ra, 24(sp)
     addi sp, sp, 32
     ret
- 
+
 # Function: insert
 # Arguments: a0 = root (struct Node*), a1 = val (int)
 # Returns: a0 = root (struct Node*)
@@ -36,45 +31,44 @@ insert:
     sd ra, 24(sp)
     sd s0, 16(sp)
     sd s1, 8(sp)
-    sd s2, 0(sp)
     mv s0, a0
     mv s1, a1
+
     bne s0, zero, insert_not_null
     mv a0, s1
-    jal make_node
+    call make_node
     j insert_done
     
 insert_not_null:
     lw t0, 0(s0)
     bge s1, t0, insert_check_right
+
     ld a0, 8(s0)
     mv a1, s1
-    jal insert
+    call insert
     sd a0, 8(s0)
     mv a0, s0
     j insert_done
     
 insert_check_right:
-    # If val > root->val, go right
     ble s1, t0, insert_equal
     ld a0, 16(s0)
     mv a1, s1
-    jal insert
+    call insert
     sd a0, 16(s0)
     mv a0, s0
     j insert_done
     
 insert_equal:
     mv a0, s0
-
+    
 insert_done:
-    ld s2, 0(sp)
     ld s1, 8(sp)
     ld s0, 16(sp)
     ld ra, 24(sp)
     addi sp, sp, 32
     ret
- 
+
 # Function: get
 # Arguments: a0 = root (struct Node*), a1 = val (int)
 # Returns: a0 = pointer to node with value val, or NULL if not found
@@ -85,32 +79,36 @@ get:
     sd s1, 8(sp)
     mv s0, a0
     mv s1, a1
+
     beq s0, zero, get_not_found
     lw t0, 0(s0)
     beq s1, t0, get_found
     bge s1, t0, get_right
-    ld a0, 8(s0)
+    ld a0, 8(s0)           # a0 = root->left
     mv a1, s1
-    jal get
+    call get
     j get_done
     
 get_right:
     ld a0, 16(s0)
     mv a1, s1
-    jal get
+    call get
     j get_done
+    
 get_found:
     mv a0, s0
     j get_done
+    
 get_not_found:
     mv a0, zero
+    
 get_done:
     ld s1, 8(sp)
     ld s0, 16(sp)
     ld ra, 24(sp)
     addi sp, sp, 32
     ret
- 
+
 # Function: getAtMost
 # Arguments: a0 = val (int), a1 = root (struct Node*)
 # Returns: a0 = greatest value in tree <= val, or -1 if none exists
@@ -120,38 +118,28 @@ getAtMost:
     sd s0, 16(sp)
     sd s1, 8(sp)
     sd s2, 0(sp)
+    
     mv s0, a0
     mv s1, a1
     li s2, -1
-    j getAtMost_helper
     
-getAtMost_helper:
+getAtMost_loop:
     beq s1, zero, getAtMost_done
     lw t0, 0(s1)
     bgt t0, s0, getAtMost_left
     mv s2, t0
     ld s1, 16(s1)
-    j getAtMost_helper
+    j getAtMost_loop
     
 getAtMost_left:
     ld s1, 8(s1)
-    j getAtMost_helper
-    
+    j getAtMost_loop
+
 getAtMost_done:
-    mv a0, s2    
+    mv a0, s2
     ld s2, 0(sp)
     ld s1, 8(sp)
     ld s0, 16(sp)
     ld ra, 24(sp)
     addi sp, sp, 32
     ret
- 
-allocate_memory:
-    la t0, heap_ptr
-    ld t1, 0(t0)           # t1 = current heap pointer (8 bytes)
-    mv t2, t1              # t2 = pointer to return
-    add t1, t1, a0         # Advance heap pointer
-    sd t1, 0(t0)           # Save new heap pointer (8 bytes)
-    mv a0, t2              # Return old heap pointer
-    ret
- 
